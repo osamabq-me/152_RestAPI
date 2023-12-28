a62ad29151a47d96575d23fd8871120c07efb1b9
OSAMAH  ALMEKHLAFI <osamah.b.ft21@mail.umy.ac.id>
2023-12-29 00:38:07 +0700

    Create the InsertViewModel

--- app/src/main/java/com/example/consumeapi/ui/kontak/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/consumeapi/ui/kontak/viewmodel/InsertViewModel.kt
@@ -1,19 +1,27 @@
 package com.example.consumeapi.ui.kontak.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.consumeapi.repository.KontakRepository
 
 
+class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel() {
+
+    var insertKontakState by mutableStateOf(InsertUiState())
+        private set
+
+    fun updateInsertKontakState(insertUiEvent: InsertUiEvent) {
+        insertKontakState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
 
 
-
-
-
-
+}
 
 data class InsertUiState(
     val  insertUiEvent: InsertUiEvent = InsertUiEvent(),
 )
-
-
 data class  InsertUiEvent(
     val id: Int = 0,
     val nama: String = "",
