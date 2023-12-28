e4d83a3b5aa13cd0e92c10e790f1514c78ddfd90
OSAMAH  ALMEKHLAFI <osamah.b.ft21@mail.umy.ac.id>
2023-12-29 00:32:47 +0700

    Crete the InsertUiState

--- app/src/main/java/com/example/consumeapi/ui/kontak/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/consumeapi/ui/kontak/viewmodel/InsertViewModel.kt
@@ -1,2 +1,22 @@
 package com.example.consumeapi.ui.kontak.viewmodel
 
+
+
+
+
+
+
+
+
+
+data class InsertUiState(
+    val  insertUiEvent: InsertUiEvent = InsertUiEvent(),
+)
+
+
+data class  InsertUiEvent(
+    val id: Int = 0,
+    val nama: String = "",
+    val email: String = "",
+    val nohp: String = "",
+)
