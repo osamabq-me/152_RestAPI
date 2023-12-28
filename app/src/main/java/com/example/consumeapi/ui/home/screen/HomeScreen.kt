a4720ce48aa916961ddff8134ce0e81c36674484
OSAMAH  ALMEKHLAFI <osamah.b.ft21@mail.umy.ac.id>
2023-12-28 23:51:50 +0700

    Add the delete ICON

--- app/src/main/java/com/example/consumeapi/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/consumeapi/ui/home/screen/HomeScreen.kt
@@ -16,12 +16,14 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -143,6 +145,15 @@ fun KontakCard(
                 text = kontak.alamat,
                 style = MaterialTheme.typography.titleMedium
             )
+
+            Spacer(Modifier.weight(1f))
+            IconButton(onClick = {onDeleteClick(kontak) }) {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = null,
+                )
+            }
+
         }
 
     }
